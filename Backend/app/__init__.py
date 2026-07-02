22bfab3796d7f1c6675f0d762a11759ef8552ad8
Huaynate Achachau José Luis <devlegendjs@gmail.com>
2026-07-01 19:57:59 -0500

    Creación de tabla facultad , usando modelos , conexion a mys , edicion de init.py para la conexionql con fichero config

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+from app.config import Config
+
+db = SQLAlchemy()
 
 
 def crear_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
     CORS(app)
+    db.init_app(app)
 
     @app.route("/")
     def inicio():
-        return "El backend esta prendido"
+        return "Api funcionando hola"
 
     return app
