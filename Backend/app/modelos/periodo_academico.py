b2289389352261dc6bc8491022c8bf8cc863b41a
Huaynate Achachau José Luis <devlegendjs@gmail.com>
2026-07-06 15:44:26 -0500

    agregar dias limite a periodo academico

--- Backend/app/modelos/periodo_academico.py
+++ Backend/app/modelos/periodo_academico.py
@@ -8,6 +8,7 @@ class PeriodoAcademico(db.Model):
     nombre = db.Column(db.String(10))
     fecha_inicio = db.Column(db.DateTime)
     fecha_fin = db.Column(db.DateTime)
+    dias_limite_pago = db.Column(db.Integer, nullable=False, default=15)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
