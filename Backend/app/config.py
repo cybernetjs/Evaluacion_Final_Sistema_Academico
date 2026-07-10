1b21ebcf1cbbf047fd99603303f0af713cb86037
Ricardo-Llanos-L <llanoslozano2005@gmail.com>
2026-07-09 21:51:05 -0500

    Cambios en el tiempo de vencimiento del token

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,3 +10,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", secrets.token_hex(32))
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
